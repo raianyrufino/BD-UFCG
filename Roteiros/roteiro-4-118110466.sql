e9c847dbc5a58f62079d9a0cfa413853fd937250
raianyrufino <raiany.paz@ccc.ufcg.edu.br>
2019-09-18 05:10:51 -0300

    roteiro04 incompleted

---
+++ Roteiros/roteiro-4-118110466.sql
@@ -0,0 +1,38 @@
+--Q1 Retornar todos os elementos da tabela departament
+
+--Q2
+
+--Q3
+
+--Q4
+
+--Q5
+
+--Q6
+
+--Q7 Retornar os nomes(primeiro e último) dos funcionários homens
+
+--Q8 Retornar os nomes dos funcionários homens que não possuem supervisor
+
+--Q9 Retornar os nomes dos funcionários supervisionados por Franklin
+
+--Q10
+
+--Q11
+
+--Q12
+
+--Q13
+
+--Q14
+
+--Q15
+
+--Q16
+
+--Q17
+
+--Q18
+
+
+

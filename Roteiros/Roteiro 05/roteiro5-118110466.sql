145825d6dd4f261c97ef83a7855fb5fa0e587ff0
raianyrufino <raiany.paz@ccc.ufcg.edu.br>
2019-09-25 04:51:27 -0300

    icpltlack of most issues to finish

--- Roteiros/Roteiro 05/roteiro5-118110466.sql
+++ Roteiros/Roteiro 05/roteiro5-118110466.sql
@@ -1,31 +1,18 @@
 -- 01 Retornar quantas funcionárias estão cadastradas
-SELECT COUNT(e) 
-FROM employee AS e
-e.sex = 'F';
+SELECT COUNT(*) FROM employee WHERE sex = 'f';
 
 -- 02 Retornar a média de salário dos funcionários homens que moram no estado do Texas
-SELECT AVG(e.salary)
-FROM employee AS e
-WHERE e.sex = 'M' AND e.adress LIKE '%TX'
+SELECT AVG(e.salary) FROM employee where sex='M' AND adress LIKE '%TX'
 
 -- 03 Retornar os ssn do supervisores e a quantidade de funcionários que cada um deles supervisiona(contar
--- também os que não são supervisionados po ninguém). Ordenar o resultado pela quantidade.
-
-SELECT e.superssn AS ssn_supervisor, COUNT(e.ssn) AS qtd_supervisionados
-FROM employee AS e 
-GROUP BY e.superssn
-ORDER BY qtd_supervisionados;
+-- também os que não são supervisionados por ninguém). Ordenar o resultado pela quantidade.
+SELECT COUNT(*) AS qtd_supervisionados, e.ssn AS ssn_supervisor FROM employee 
+				AS r RIGHT OUTER JOIN employee AS r ON e.ssn = r.superssn GROUP BY r.ssn ORDER BY qtd_supervisionados ASC; 
 
 -- 04 Para cada funcionário que supervisiona alguém, retornar seu nome e a quantidade de funcionários que 
 -- supervisiona. O resultado deve ser ordenado pela quantidade de funcionários supervisionados.
-
-SELECT s.fname AS nome_supervisor, COUNT(*) AS qtd_supervisionados
-FROM employee AS s INNER JOIN employee AS e ON s.ssn = e.superssn
-GROUP BY s.fname
-ORDER BY qtd_supervisionados;
+SELECT s.fname AS fname_supervisor, COUNT(*) AS qtd_supervisionados FROM employee AS s JOIN employee AS e ON 
 
 -- 05 Faça uma consulta equivalente à anterior, porém considerando os funcionários que não possuem supervisor.
-SELECT s.fname AS nome_supervisor, COUNT(*) AS qtd_supervisionados
-FROM employee AS s RIGHT OUTER JOIN employee AS e ON s.ssn = e.superssn
-GROUP BY s.fname
-ORDER BY qtd_supervisionados;
+
+SELECT S.fname AS nome_supervisor, COUNT(*) AS qtd_supervisionados FROM employee AS S JOIN employee AS E ON E.superssn = S.ssn GROUP BY S.fname ORDER BY qtd_supervisionados ASC;

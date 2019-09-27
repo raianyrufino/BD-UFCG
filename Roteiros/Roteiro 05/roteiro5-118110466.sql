2f8b3450334250702631c137ff1c571cb63010d2
raianyrufino <raiany.paz@ccc.ufcg.edu.br>
2019-09-26 22:53:01 -0300

    R05

--- Roteiros/Roteiro 05/roteiro5-118110466.sql
+++ Roteiros/Roteiro 05/roteiro5-118110466.sql
@@ -6,15 +6,138 @@ SELECT AVG(e.salary) FROM employee where sex='M' AND adress LIKE '%TX'
 
 -- 03 Retornar os ssn do supervisores e a quantidade de funcionários que cada um deles supervisiona(contar
 -- também os que não são supervisionados por ninguém). Ordenar o resultado pela quantidade.
-SELECT COUNT(*) AS qtd_supervisionados, e.ssn AS ssn_supervisor FROM employee 
-				AS r RIGHT OUTER JOIN employee AS r ON e.ssn = r.superssn GROUP BY r.ssn ORDER BY qtd_supervisionados ASC; 
+SELECT
+    s.ssn AS ssn_supervisor,
+    COUNT(*) AS qtd_supervisionados
+FROM
+    employee AS e
+    LEFT JOIN employee AS s ON e.superssn = s.ssn
+GROUP BY
+    ssn_supervisor
+ORDER BY
+    qtd_supervisionados;
 
 -- 04 Para cada funcionário que supervisiona alguém, retornar seu nome e a quantidade de funcionários que 
 -- supervisiona. O resultado deve ser ordenado pela quantidade de funcionários supervisionados.
-SELECT s.fname AS fname_supervisor, COUNT(*) AS qtd_supervisionados FROM employee AS s JOIN employee AS e ON 
+SELECT
+    s.fname AS nome_supervisor,
+    COUNT(*) AS qtd_supervisionados
+FROM
+    employee AS e
+    INNER JOIN employee AS s ON s.ssn = e.superssn
+GROUP BY
+    s.fname
+ORDER BY
+    COUNT(*);
+
 
 -- 05 Faça uma consulta equivalente à anterior, porém considerando os funcionários que não possuem supervisor.
 
-SELECT S.fname AS nome_supervisor, COUNT(*) AS qtd_supervisionados FROM employee AS S JOIN employee AS E ON E.superssn = S.ssn GROUP BY S.fname ORDER BY qtd_supervisionados ASC;
+SELECT
+    s.fname AS nome_supervisor,
+    COUNT(*) AS qtd_supervisionados
+FROM
+    employee AS e
+    LEFT JOIN employee AS s ON s.ssn = e.superssn
+GROUP BY
+    s.fname
+ORDER BY
+    COUNT(*);
 
 -- 06
+
+SELECT
+    MIN(qtd) AS qtd_empregados
+FROM
+    (
+        SELECT
+            COUNT(essn) AS qtd
+        FROM
+            works_on
+        GROUP BY
+            pno
+    ) as qtds;
+
+
+--QUESTAO 7--
+SELECT pno AS proj,
+       COUNT(essn) AS qtd
+FROM works_on
+
+GROUP BY pno
+
+HAVING COUNT(essn) = (
+    SELECT MIN(qtd) AS qtd_empregados
+    FROM (
+        SELECT
+            COUNT(essn) AS qtd
+        FROM
+            works_on
+        GROUP BY
+            pno
+    ) as qtds);       
+
+
+--QUESTAO 8--
+SELECT
+    w.pno AS num_proj,
+    AVG(e.salary)
+FROM
+    works_on as w,
+    employee as e
+WHERE
+    e.ssn = w.essn
+GROUP BY
+    pno;
+--QUESTAO 9-- !!
+SELECT
+    w.pno AS proj_num,
+    p.pname AS proj_nome,
+    AVG(e.salary) AS media_sal
+FROM
+    works_on as w,
+    employee as e,
+    project as p
+WHERE
+    e.ssn = w.essn
+    AND w.pno = p.pnumber
+GROUP BY
+    pno;
+--QUESTAO 10--
+SELECT
+    distinct e.fname,
+    e.salary
+FROM
+    works_on AS w,
+    employee AS e
+WHERE
+    e.ssn NOT IN (
+        SELECT
+            w.essn
+        FROM
+            works_on AS w
+        WHERE
+            w.pno = 92
+    )
+    AND e.salary > (
+        SELECT
+            MAX(salary)
+        FROM
+            works_on AS w,
+            employee AS e
+        WHERE
+            w.essn = e.ssn
+            AND w.pno = 92
+    );
+--QUESTAO 11--
+SELECT
+    e.ssn,
+    COUNT(w.essn) AS qtd_proj
+FROM
+    employee as e
+    LEFT JOIN works_on AS w on e.ssn = w.essn
+GROUP BY
+    e.ssn
+ORDER BY
+    COUNT(w.essn);
+
